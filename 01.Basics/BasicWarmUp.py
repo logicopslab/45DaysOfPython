81bf39ddcf0cf2c7cd1d207f8f20a660ef58cea9
logicopslab <logicopslab@gmail.com>
2026-03-01 23:38:19 -0600

    Very Basic WarmUp

---
+++ 01.Basics/BasicWarmUp.py
@@ -0,0 +1,13 @@
+a = 10;
+b = 20;
+c = 30;
+
+print ("Sum of all 3 is :", a+b+c)
+print ("Delete b from c is:", c-b)
+print ("Multipication of all 3 is :", a*b*c)
+
+print("a > b:", a > b)
+print("b > a:", b > a)
+
+print("c / a:", c/a)
+print("c % 8:", c%8)
