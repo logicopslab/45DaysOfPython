0abb8bdcc2d5edba04d7760e4af6a17f776ebd82
LogicOps Lab by Ravish <82759985+logicopslab@users.noreply.github.com>
2026-03-06 22:17:57 -0800

    Create IndexesSimple.py

---
+++ 04.IndexesAndSlicing/IndexesSimple.py
@@ -0,0 +1,16 @@
+# List of fruits
+fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"]
+
+# Access using positive indexes
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+print("Third fruit:", fruits[2])
+
+# Access using negative indexes
+print("Last fruit:", fruits[-1])
+print("Second last fruit:", fruits[-2])
+
+# Modify value using index
+fruits[1] = "Pineapple"
+
+print("Updated list:", fruits)
